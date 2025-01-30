065df70b9a4e63f1312c50bed285c0f838503fd7
DiegoscomV3 <opgems9000@gmail.com>
2025-01-30 13:19:37 +0100

    fix float inf overflow

--- ai.pl
+++ ai.pl
@@ -16,18 +16,18 @@ random_move(B,_, I) :-
 depth(4). % Profondeur maximale de calcul
 
 % Afin de déterminer les bornes de l algorithme minmax, 
-% On utilisera 1.7976931348623157e+308 pour +infini et 2.2250738585072014e-308 pour -infini
+% On utilisera 100000 pour +infini et -100000 pour -infini
 
 
 minmax_winnings_move(B, Player, COL):-
     depth(D),
-    minmax(B, D, Player, Player, utility_4aligned, 2.2250738585072014e-308, 1.7976931348623157e+308, U, COL).
+    minmax(B, D, Player, Player, utility_4aligned, (-100000), 1000000, U, COL).
 
 minmax_naif_move(B, Player, COL):-
     depth(D),
-    minmax(B, D, Player, Player, utility_naif, 2.2250738585072014e-308, 1.7976931348623157e+308, _, COL).
+    minmax(B, D, Player, Player, utility_naif, (-100000), 1000000, _, COL).
 
 minmax_strategique_move(B, Player, COL):-
     depth(D),
-    minmax(B, D, Player, Player, utility_strategique, 2.2250738585072014e-308, 1.7976931348623157e+308, _, COL).
+    minmax(B, D, Player, Player, utility_strategique, (-100000), 1000000, _, COL).
 
